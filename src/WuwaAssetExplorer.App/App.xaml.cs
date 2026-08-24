a017a0a723348e8d38102c76df639e8b3e4a6d3a
shmilyfuu <47942250+shmilyfuu@users.noreply.github.com>
2026-08-25 00:50:43 +0800

    Log WinUI app activation stages

--- src/WuwaAssetExplorer.App/App.xaml.cs
+++ src/WuwaAssetExplorer.App/App.xaml.cs
@@ -8,12 +8,23 @@ public partial class App : Application
 
     public App()
     {
+        StartupLog.Write("App constructor entered");
         InitializeComponent();
+        UnhandledException += App_UnhandledException;
+        StartupLog.Write("App XAML initialized");
     }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
+        StartupLog.Write("OnLaunched entered");
         _window = new MainWindow();
+        StartupLog.Write("MainWindow constructed");
         _window.Activate();
+        StartupLog.Write("MainWindow activated");
+    }
+
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        StartupLog.Write("WinUI unhandled exception", e.Exception);
     }
 }
